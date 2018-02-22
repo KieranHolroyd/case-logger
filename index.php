1d16de12d65eb13e5450f06882200614080cc734
Unknown <kieran@nitrexdesign.co.uk>
2018-02-22 16:45:57 +0000

    CAAAAAATS!!!

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php $nonav=0; include "head.php"; ?>
-<body>
-	<div>
+<body style="background: no-repeat center center fixed;background-size: cover;background-image: url(https://thecatapi.com/api/images/get?format=src&type=png);">
+	<div id="titleText">
 		<h1 id="welcome" style="padding: 10px;display:inline-block;">Hello, Human</h1><h1 style="padding: 10px;float:right;display:inline-block;" id="dtime"></h1>
 	</div>
   <a href="logger">
@@ -64,6 +64,64 @@
 		setInterval(() => {
 			$('#dtime').text(currentTime());
 		}, 1000);
+		function getBGImg() {
+			var bg = $('body').css('background-image');
+			bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+			return bg;
+		}
+		function getImageBrightness(imageSrc,callback) {
+			var img = document.createElement("img");
+			img.src = imageSrc;
+			img.style.display = "none";
+			img.crossOrigin = '';
+			document.body.appendChild(img);
+
+			var colorSum = 0;
+
+			img.onload = function() {
+				// create canvas
+				var canvas = document.createElement("canvas");
+				canvas.width = this.width;
+				canvas.height = this.height;
+
+				var ctx = canvas.getContext("2d");
+				ctx.drawImage(this,0,0);
+
+				var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+				var data = imageData.data;
+				var r,g,b,avg;
+
+				for(var x = 0, len = data.length; x < len; x+=4) {
+					r = data[x];
+					g = data[x+1];
+					b = data[x+2];
+
+					avg = Math.floor((r+g+b)/3);
+					colorSum += avg;
+				}
+
+				var brightness = Math.floor(colorSum / (this.width*this.height));
+				callback(brightness);
+			}
+		}
+		getImageBrightness(getBGImg(),function(brightness) {
+			console.warn(brightness);
+			if(brightness > 100){
+				setColor(0);
+			}
+		});
+		function setColor(val) {
+			switch (val) {
+				case 1:
+					$('#titleText h1').css('color', '#ddd');
+					$('#titleText p').css('color', '#ddd');
+					break;
+				case 0:
+					$('#titleText h1').css('color', '#333');
+					$('#titleText p').css('color', '#333');
+					break;
+			}
+		}
 	</script>
 </body>
 <!--Created By Kieran Holroyd-->
