1f6db48690f8746105a953bdc4f92f8f792e29c9
Unknown <kieran@nitrexdesign.co.uk>
2018-02-22 17:00:07 +0000

    removed super cool text color changer for dumb overlay :(

--- index.php
+++ index.php
@@ -66,64 +66,6 @@
 		setInterval(() => {
 			$('#dtime').text(currentTime());
 		}, 1000);
-		function getBGImg() {
-			var bg = $('body').css('background-image');
-			bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
-			return bg;
-		}
-		function getImageBrightness(imageSrc,callback) {
-			var img = document.createElement("img");
-			img.src = imageSrc;
-			img.style.display = "none";
-			img.crossOrigin = '';
-			document.body.appendChild(img);
-
-			var colorSum = 0;
-
-			img.onload = function() {
-				// create canvas
-				var canvas = document.createElement("canvas");
-				canvas.width = this.width;
-				canvas.height = this.height;
-
-				var ctx = canvas.getContext("2d");
-				ctx.drawImage(this,0,0);
-
-				var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-				var data = imageData.data;
-				var r,g,b,avg;
-
-				for(var x = 0, len = data.length; x < len; x+=4) {
-					r = data[x];
-					g = data[x+1];
-					b = data[x+2];
-
-					avg = Math.floor((r+g+b)/3);
-					colorSum += avg;
-				}
-
-				var brightness = Math.floor(colorSum / (this.width*this.height));
-				callback(brightness);
-			}
-		}
-		getImageBrightness(getBGImg(),function(brightness) {
-			console.warn(brightness);
-			if(brightness > 125){
-				setColor(0);
-			}
-		});
-		function setColor(val) {
-			switch (val) {
-				case 1:
-					$('#titleText h1').css('color', '#fff');
-					$('#titleText p').css('color', '#fff');
-					break;
-				case 0:
-					$('#titleText h1').css('color', '#333');
-					$('#titleText p').css('color', '#333');
-					break;
-			}
-		}
 	</script>
 </body>
 <!--Created By Kieran Holroyd-->
